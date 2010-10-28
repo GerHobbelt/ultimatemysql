86196820f416fb070d229c735509bb68211a564b
Ger Hobbelt <ger@hobbelt.com>
2010-10-29 01:49:27 +0200

    Security fixes part I: define("COMPACTCMS_CODE") + BASE_PATH
    All php entry point files will
      define("COMPACTCMS_CODE", 1);
    while all non-entry point php files will check for this define and b0rk with an error ASAP to prevent attackers from executing php code in undesirable ways/order.
    
    Together with this comes a consistency update where each entry point PHP file will make sure that the 'BASE_PATH' define is set, which can be used in PHP require/require_once/include statements.

--- mysql.class.php
+++ mysql.class.php
@@ -1,5 +1,9 @@
 <?php
- /**
+
+/* make sure no-one can run anything here if they didn't arrive through 'proper channels' */
+if(!defined("COMPACTCMS_CODE")) die('Illegal entry point!');
+
+/**
  * Copyright (C) 2008 - 2010 by Xander Groesbeek (CompactCMS.nl)
  * 
  * Last changed: $LastChangedDate$
