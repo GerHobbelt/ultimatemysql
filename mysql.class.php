f15fa48c2a2ab2570d2cad6513b2a7b3d5b4f1ae
Ger Hobbelt <ger@hobbelt.com>
2010-11-01 08:30:13 +0100

    security part III : die horribly when you're not supposed to start with this PHP file...
    Introduced a
      if !defined(SPECIAL_MARKER) die("horribly!");
    mechanism like many other PHP projects out there already sport a long time.
    The alternative would have been .htaccess, but that one's granularity is per-dir which is a no-go for us.

--- mysql.class.php
+++ mysql.class.php
@@ -1,7 +1,7 @@
 <?php
 
 /* make sure no-one can run anything here if they didn't arrive through 'proper channels' */
-if(!defined("COMPACTCMS_CODE")) die('Illegal entry point!');
+if(!defined("COMPACTCMS_CODE")) { die('Illegal entry point!'); } /*MARKER*/
 
 /**
  * Ultimate MySQL Wrapper Class
